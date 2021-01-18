4f29744c76b94cd0be0619b386a95f2b47f7f9a9
Ambroise Maupate <ambroise@rezo-zero.com>
2021-01-18 20:09:14 +0100

    Move backoffice.entries to service provider

--- src/AbstractApiThemeTrait.php
+++ src/AbstractApiThemeTrait.php
@@ -13,24 +13,5 @@ trait AbstractApiThemeTrait
     public static function setupDependencyInjection(Container $container)
     {
         parent::setupDependencyInjection($container);
-
-        $container->extend('backoffice.entries', function (array $entries, Container $c) {
-            $entries['api'] = [
-                'name' => 'api.menu',
-                'path' => null,
-                'icon' => 'uk-icon-gears',
-                'roles' => ['ROLE_ADMIN_API'],
-                'subentries' => [
-                    'applications' => [
-                        'name' => 'api.menu.applications',
-                        'path' => $c['urlGenerator']->generate('adminApiApplications'),
-                        'icon' => 'uk-icon-gears',
-                        'roles' => ['ROLE_ADMIN_API'],
-                    ],
-                ]
-            ];
-
-            return $entries;
-        });
     }
 }
--- src/Services/AbstractApiServiceProvider.php
+++ src/Services/AbstractApiServiceProvider.php
@@ -472,5 +472,24 @@ class AbstractApiServiceProvider implements ServiceProviderInterface
             );
             return $firewallMap;
         });
+
+        $container->extend('backoffice.entries', function (array $entries, Container $c) {
+            $entries['api'] = [
+                'name' => 'api.menu',
+                'path' => null,
+                'icon' => 'uk-icon-gears',
+                'roles' => ['ROLE_ADMIN_API'],
+                'subentries' => [
+                    'applications' => [
+                        'name' => 'api.menu.applications',
+                        'path' => $c['urlGenerator']->generate('adminApiApplications'),
+                        'icon' => 'uk-icon-gears',
+                        'roles' => ['ROLE_ADMIN_API'],
+                    ],
+                ]
+            ];
+
+            return $entries;
+        });
     }
 }
