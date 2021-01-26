9b2ff235650a45aa014660c9c7521db0b3cf6119
Ambroise Maupate <ambroise@rezo-zero.com>
2021-01-26 18:39:31 +0100

    Public method to generate cache tags strings

--- src/Cache/CacheTagsCollection.php
+++ src/Cache/CacheTagsCollection.php
@@ -10,27 +10,42 @@ use RZ\Roadiz\Core\Entities\Tag;
 
 final class CacheTagsCollection extends ArrayCollection
 {
+    public function getCacheTagForNode(Node $node): string
+    {
+        return 'n'.$node->getId();
+    }
+
     public function addNode(Node $node): CacheTagsCollection
     {
-        $cacheTag = 'n'.$node->getId();
+        $cacheTag = $this->getCacheTagForNode($node);
         if (!$this->contains($cacheTag)) {
             $this->add($cacheTag);
         }
         return $this;
+    }
+
+    public function getCacheTagForTag(Tag $tag): string
+    {
+        return 't'.$tag->getId();
     }
 
     public function addTag(Tag $tag): CacheTagsCollection
     {
-        $cacheTag = 't'.$tag->getId();
+        $cacheTag = $this->getCacheTagForTag($tag);
         if (!$this->contains($cacheTag)) {
             $this->add($cacheTag);
         }
         return $this;
     }
 
+    public function getCacheTagForDocument(Document $document): string
+    {
+        return 'd'.$document->getId();
+    }
+
     public function addDocument(Document $document): CacheTagsCollection
     {
-        $cacheTag = 'd'.$document->getId();
+        $cacheTag = $this->getCacheTagForDocument($document);
         if (!$this->contains($cacheTag)) {
             $this->add($cacheTag);
         }
